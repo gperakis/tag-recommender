5f492bfed1b9a8e4538a6f35ea89e7cdbd4dbc2d
George Perakis <george.perakis@aisera.com>
2024-10-03 23:27:26 +0300

    Update CLI to include data splitting

--- tag_recommender/__main__.py
+++ tag_recommender/__main__.py
@@ -1,6 +1,9 @@
-import click
 from pathlib import Path
+
+import click
+
 from tag_recommender.logger import initialize_logging
+from tag_recommender.process.cli import data
 from tag_recommender.service.cli import services
 
 
@@ -12,6 +15,7 @@ def cli():
 
 # attach retrieval services cli
 cli.add_command(services)
+cli.add_command(data)
 
 if __name__ == "__main__":
     cli()
