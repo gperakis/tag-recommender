c233ad0c33df189ea6f7c993a09d5b9cbc7c11f5
George Perakis <george.perakis@aisera.com>
2024-10-10 00:25:58 +0300

    Update text utils

--- tag_recommender/utils/text.py
+++ tag_recommender/utils/text.py
@@ -1,6 +1,56 @@
 import re
+import unicodedata
 
+import emoji
 from tqdm import tqdm
+
+
+def remove_emojis(text):
+    return emoji.replace_emoji(text, "")
+
+
+def to_snake_case_boosted(text: str) -> str:
+    """
+    Convert a string to snake_case, supporting Unicode characters,
+    handling punctuation, emoticons, and other special cases related to hashtags.
+
+    Parameters
+    ----------
+    text : str
+        The input string to convert.
+
+    Returns
+    -------
+    str
+        The input string converted to snake_case.
+    """
+    # Normalize Unicode characters to NFKD form
+    text = unicodedata.normalize("NFKD", text)
+
+    # Remove accents from characters
+    text = "".join([c for c in text if not unicodedata.combining(c)])
+
+    # Remove emojis using the emoji library
+    text = remove_emojis(text)
+
+    # Remove punctuation marks, keeping underscores if needed
+    text = re.sub(r"[^\w\s]", "", text, flags=re.UNICODE)
+
+    # Replace spaces and hyphens with underscores
+    text = re.sub(r"[\s\-]+", "_", text, flags=re.UNICODE)
+
+    # Handle CamelCase or PascalCase
+    text = re.sub(r"(?<=[a-z0-9])(?=[A-Z])", "_", text)
+    text = re.sub(r"(?<=[A-Z])(?=[A-Z][a-z0-9])", "_", text)
+
+    # Convert to lowercase
+    text = text.lower()
+
+    # Remove multiple underscores
+    text = re.sub(r"_+", "_", text)
+
+    # Strip leading and trailing underscores and whitespace
+    return text.strip("_").strip()
 
 
 def split_tags(tags_str: str) -> list[str]:
@@ -71,7 +121,7 @@ def normalize_hashtags(text: str) -> list[str]:
 
     tags = text.split(",")
 
-    return [to_snake_case(hashtag) for hashtag in tags]
+    return [to_snake_case_boosted(hashtag) for hashtag in tags]
 
 
 def preprocess_corpus(corpus: list[str]) -> list[list[str]]:
