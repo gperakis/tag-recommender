5ba32ec3ced86255496611d9cb4199fccc49c532
gperakis <perakisgeorgios@gmail.com>
2024-10-11 11:45:57 +0300

    Update rest settings

--- tag_recommender/config.py
+++ tag_recommender/config.py
@@ -1,8 +1,10 @@
+from pathlib import Path
+
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
-class Settings(BaseSettings):
+class AppSettings(BaseSettings):
     app_name: str = "Tag Recommender API"
     app_version: str = "1.0.0"
     allowed_origins: list[str] = ["*"]
@@ -17,4 +19,45 @@ class Settings(BaseSettings):
         env_file = ".env"
 
 
-settings = Settings()
+class Tag2VecSettings(BaseSettings):
+    vector_size: int = 100
+    window: int = 3
+    min_count: int = 5
+    workers: int = 16
+    sg: int = 1  # Skip-gram. If 0, then CBOW.
+    epochs: int = 200
+    sorted_vocab: int = 1
+
+
+class CountMinSketchSettings(BaseSettings):
+    depth: int = 5
+    width: int = 1000
+    seed: int = 42
+
+
+class ModelSettings(BaseSettings):
+    input_file: str = Field(
+        default="data/full_dataset.csv", description="Path to the input file."
+    )
+    normalize: bool = Field(
+        default=True, description="Whether to normalize tags during preprocessing."
+    )
+    datasets_dir: Path | None = Field(
+        default=Path("artifacts", "datasets"),
+        description="Directory to save split datasets.",
+    )
+    save_dir: Path | None = Field(
+        default=Path("artifacts", "models"),
+        description="Directory to save split datasets.",
+    )
+    random_state: int = Field(
+        default=42, description="Random seed for reproducibility."
+    )
+    train_size: float = Field(default=0.8, description="Training set size.")
+    val_size: float = Field(default=0.10, description="Validation set size.")
+    test_size: float = Field(default=0.10, description="Test set size.")
+
+
+app_settings = AppSettings()
+tag2vec_settings = Tag2VecSettings()
+model_settings = ModelSettings()
