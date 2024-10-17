1e5805e97aa91c93380ef29211675f89dbc2471e
gperakis <perakisgeorgios@gmail.com>
2024-10-18 00:41:18 +0300

    Incorporate tag2vec model into the inference engine

--- tag_recommender/recommend/factories.py
+++ tag_recommender/recommend/factories.py
@@ -1,9 +1,11 @@
 from tag_recommender.infer import MultiModelInference
 from tag_recommender.recommend.co_occur.factories import load_co_occur_inference_model
+from tag_recommender.recommend.tag2vec.factories import load_tag2vec_model
 
 
 def create_inference_engine() -> MultiModelInference:
     # Initialize the inference engine
     co_occur = load_co_occur_inference_model()
-    inference_engine = MultiModelInference([co_occur])
+    tag2vec = load_tag2vec_model()
+    inference_engine = MultiModelInference([co_occur, tag2vec])
     return inference_engine
