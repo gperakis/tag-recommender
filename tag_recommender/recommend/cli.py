282c40268b90c23e81746661b9a50699b51f9809
gperakis <perakisgeorgios@gmail.com>
2024-10-18 14:45:44 +0300

    Update training CLI

--- tag_recommender/recommend/cli.py
+++ tag_recommender/recommend/cli.py
@@ -2,7 +2,9 @@ import logging
 
 import click
 
+from tag_recommender.recommend.association_rules.factories import train_tag_rules_model
 from tag_recommender.recommend.co_occur.factories import train_co_occur_model
+from tag_recommender.recommend.tag2vec.factories import train_tag2vec_model
 
 logger = logging.getLogger(__name__)
 
@@ -19,3 +21,21 @@ def train_co_occurrence():
     """
     logger.info("Training co-occurrence model...")
     train_co_occur_model()
+
+
+@models.command()
+def train_tag2vec():
+    """
+    Train a tag2vec model.
+    """
+    logger.info("Training tag2vec model...")
+    train_tag2vec_model()
+
+
+@models.command()
+def train_tag_rules():
+    """
+    Train a tag rules model.
+    """
+    logger.info("Training Tag Association Rules model...")
+    train_tag_rules_model()
