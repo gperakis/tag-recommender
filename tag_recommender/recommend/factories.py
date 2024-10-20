73a1e33928fa7f80c2dc9750cbd8ed827d12ea41
gperakis <perakisgeorgios@gmail.com>
2024-10-21 00:40:43 +0300

    Include the association rules model to the inference engine

--- tag_recommender/recommend/factories.py
+++ tag_recommender/recommend/factories.py
@@ -1,4 +1,5 @@
 from tag_recommender.infer import MultiModelInference
+from tag_recommender.recommend.association_rules.factories import load_tag_rules_model
 from tag_recommender.recommend.co_occur.factories import load_co_occur_inference_model
 from tag_recommender.recommend.tag2vec.factories import load_tag2vec_model
 
@@ -7,5 +8,6 @@ def create_inference_engine() -> MultiModelInference:
     # Initialize the inference engine
     co_occur = load_co_occur_inference_model()
     tag2vec = load_tag2vec_model()
-    inference_engine = MultiModelInference([co_occur, tag2vec])
+    tag_rules = load_tag_rules_model()
+    inference_engine = MultiModelInference([co_occur, tag2vec, tag_rules])
     return inference_engine
